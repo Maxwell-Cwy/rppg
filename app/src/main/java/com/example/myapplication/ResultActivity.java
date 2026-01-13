7ef6c535e837b63f9ec9cebb2180cac17b436b94
LHY <lhy@163.com>
2026-01-13 20:20:17 +0800

    202601132020

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -9,17 +9,17 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+        setContentView(R.layout.activity_report);
 
-        TextView tvResult = findViewById(R.id.tv_result);
-        String report = getIntent().getStringExtra("REPORT");
-        String videoPath = getIntent().getStringExtra("VIDEO_PATH");
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("✅ 检测已完成！\n\n");
-        if (report != null) sb.append(report).append("\n\n");
-        if (videoPath != null) sb.append("视频路径：").append(videoPath);
-
-        tvResult.setText(sb.toString());
+//        TextView tvResult = findViewById(R.id.tv_result);
+//        String report = getIntent().getStringExtra("REPORT");
+//        String videoPath = getIntent().getStringExtra("VIDEO_PATH");
+//
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("✅ 检测已完成！\n\n");
+//        if (report != null) sb.append(report).append("\n\n");
+//        if (videoPath != null) sb.append("视频路径：").append(videoPath);
+//
+//        tvResult.setText(sb.toString());
     }
 }
